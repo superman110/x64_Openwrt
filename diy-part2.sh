dcec4371c6e15f0336c625b7e8ae93afdcdbae44
superman110 <superman110@gmail.com>
2022-10-16 03:15:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -86,7 +86,7 @@ svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/
 #mosdns
 rm -rf feeds/packages/net/mosdns
 svn co https://github.com/sbwml/luci-app-mosdns package/mosdns
-svn co clone https://github.com/sbwml/v2ray-geodata package/geodata
+svn co https://github.com/sbwml/v2ray-geodata package/geodata
 #svn co https://github.com/kenzok8/small-package/trunk/mosdns package/mosdns
 #svn co https://github.com/kenzok8/small-package/trunk/luci-app-mosdns package/luci-app-mosdns
 
