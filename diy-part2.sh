129309d9749b7058857f6bc8696b3d4881222706
superman110 <superman110@gmail.com>
2023-11-27 22:11:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,43 +38,8 @@ sed -i '$ a net.netfilter.nf_conntrack_tcp_timeout_last_ack=10' package/base-fil
 sed -i '$ a net.core.somaxconn=65535' package/base-files/files/etc/sysctl.conf
 
 
-#添加额外软件包
-
-svn co https://github.com/kenzok8/small-package/trunk/luci-app-bypass package/luci-app-bypass
-sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-bypass/Makefile
-
-svn co https://github.com/kenzok8/small-package/trunk/brook package/brook
-svn co https://github.com/kenzok8/small-package/trunk/chinadns-ng package/chinadns-ng
-svn co https://github.com/kenzok8/small-package/trunk/dns2socks package/dns2socks
-svn co https://github.com/kenzok8/small-package/trunk/dns2tcp package/dns2tcp
-svn co https://github.com/kenzok8/small-package/trunk/hysteria package/hysteria
-svn co https://github.com/kenzok8/small-package/trunk/ipt2socks package/ipt2socks
-svn co https://github.com/kenzok8/small-package/trunk/pdnsd-alt package/pdnsd-alt
-svn co https://github.com/kenzok8/small-package/trunk/shadowsocks-libev package/shadowsocks-libev
-svn co https://github.com/kenzok8/small-package/trunk/shadowsocks-rust package/shadowsocks-rust
-svn co https://github.com/kenzok8/small-package/trunk/shadowsocksr-libev package/shadowsocksr-libev
-svn co https://github.com/kenzok8/small-package/trunk/simple-obfs package/simple-obfs
-svn co https://github.com/kenzok8/small-package/trunk/ssocks package/ssocks
-svn co https://github.com/kenzok8/small-package/trunk/trojan-go package/trojan-go
-svn co https://github.com/kenzok8/small-package/trunk/trojan-plus package/trojan-plus
-svn co https://github.com/kenzok8/small-package/trunk/trojan package/trojan
-svn co https://github.com/kenzok8/small-package/trunk/v2ray-core package/v2ray-core
-svn co https://github.com/kenzok8/small-package/trunk/v2ray-geodata package/v2ray-geodata
-svn co https://github.com/kenzok8/small-package/trunk/v2ray-plugin package/v2ray-plugin
-svn co https://github.com/kenzok8/small-package/trunk/v2raya package/v2raya
-svn co https://github.com/kenzok8/small-package/trunk/xray-core package/xray-core
-svn co https://github.com/kenzok8/small-package/trunk/xray-plugin package/xray-plugin
-
-svn co https://github.com/kenzok8/small-package/trunk/luci-lib-taskd package/luci-lib-taskd
-svn co https://github.com/kenzok8/small-package/trunk/luci-lib-xterm package/luci-lib-xterm
-svn co https://github.com/kenzok8/small-package/trunk/taskd package/taskd
-svn co https://github.com/kenzok8/small-package/trunk/lua-neturl package/lua-neturl
-svn co https://github.com/kenzok8/small-package/trunk/lua-maxminddb package/lua-maxminddb
-svn co https://github.com/kenzok8/small-package/trunk/microsocks package/microsocks
-svn co https://github.com/kenzok8/small-package/trunk/gn package/gn
-svn co https://github.com/kenzok8/small-package/trunk/naiveproxy package/naiveproxy
-svn co https://github.com/kenzok8/small-package/trunk/redsocks2 package/redsocks2
-svn co https://github.com/kenzok8/small-package/trunk/tcping package/tcping
+#添加bypass
+svn export https://github.com/haiibo/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 
 #添加openclash
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
@@ -123,6 +88,8 @@ svn export https://github.com/linkease/istore/trunk/luci package/luci-app-store
 # luci-app-irqbalance
 svn co https://github.com/QiuSimons/OpenWrt-Add/trunk/luci-app-irqbalance package/luci-app-irqbalance
 sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
+
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
